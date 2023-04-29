044e4ab94509c50c139bf8bd1c786b6eb1053271
msjiejiejie <maxieren@gmail.com>
2023-04-29 19:28:30 +1000

    backend couchDB init

---
+++ couchDB/Vue/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import FirstView from '@/components/FirstView.vue'
+import SecondView from '@/components/SecondView.vue'
+import ThirdView from '@/components/ThirdView.vue'
+
+const routes = [
+    {
+      path: '/view1',
+      name: 'FirstView',
+      component: FirstView
+    },
+    {
+      path: '/view2',
+      name: 'SecondView',
+      component: SecondView
+    },
+    {
+      path: '/view3',
+      name: 'ThirdView',
+      component: ThirdView
+    }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
+
